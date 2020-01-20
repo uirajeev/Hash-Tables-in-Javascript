22d9d5039fb0bd1e33bdb5b390b7e34dbfec4cd2
Rajeev Kumar Singh <rajeevkumar.web@gmail.com>
2020-01-20 07:39:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Hash table Constructor 
+function HashTable(size) {
+  this.bucket = new Array(size);
+  this.bucketSize = this.bucket.length;
+}
+
+function HashNode(key, value, next) {
+  this.key = key;
+  this.value = value;
+  this.next = next || null;
+}
+
+// Hashing function 
+HashTable.prototype.hash = function(key) {
+  var total = 0;
+  var size = key.length;
+  while(size--) {
+    total += key.charCodeAt(size);
+  }
+ return total % this.bucketSize;
+}
+
+var HT = new HashTable(123);
+console.log(HT.hash("eh"));
+console.log(HT);
